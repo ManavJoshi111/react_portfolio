83a71f33ebb5a73e4e7c03271700d0480f8a2654
ManavJoshi <manavjoshi154@gmail.com>
2022-05-08 19:45:56 +0530

    readme deleted

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 const App = () => {
-  return <h1 align="center">Now it is working</h1>;
+  return <h1 align="center">Manav Portfolio</h1>;
 };
 
 export default App;
