a03c880804981704d8aa41e864f7f53b313bd748
ManavJoshi <manavjoshi154@gmail.com>
2023-05-24 16:17:04 +0530

    Added logic to show projects directly via a link

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,31 +1,49 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import Card from './Card';
 import projects from '../Data/projects';
-function Projects() {
+
+const Projects = () => {
+    const location = useLocation();
+
+    useEffect(() => {
+        const { hash } = location;
+        if (hash === '#projects') {
+            const projectsSection = document.getElementById('projects');
+            projectsSection.scrollIntoView({ behavior: 'smooth' });
+        }
+    }, [location]);
+
     return (
         <>
-            <center><h2 className="titleproj text-light" id="projects"><u>Projects - Made By Me</u></h2></center>
-            <div className="container-fluid mt-4">
+            <center>
+                <h2 className="titleproj text-light" id="projects">
+                    <u>Projects - Made By Me</u>
+                </h2>
+            </center>
+            <div className="container-fluid mt-3">
                 <div className="row">
                     <div className="row">
-                        {
-                            projects.map((val, ind) => {
-                                // the projects has fields like title, description, technologies (an array), code_url and live_url, display only if the field is not empty, display it like a card
-                                return (
-                                    <>
-                                        <div className="col-md-6">
-                                            {(val.title !== "" && val.title !== undefined) ?
-                                                <Card key={ind} title={val.title} description={val.description} technologies={val.technologies} code_url={val.code_url} live_url={val.live_url} /> : ""}
-                                        </div>
-                                    </>
-                                )
-                            })
-                        }
+                        {projects.map((val, ind) => {
+                            return (
+                                <div className="col-md-6" key={ind}>
+                                    {val.title !== '' && val.title !== undefined && (
+                                        <Card
+                                            title={val.title}
+                                            description={val.description}
+                                            technologies={val.technologies}
+                                            code_url={val.code_url}
+                                            live_url={val.live_url}
+                                        />
+                                    )}
+                                </div>
+                            );
+                        })}
                     </div>
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
 
 export default Projects;
