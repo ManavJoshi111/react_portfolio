5fff63f16618c3d87c5f47a7febd1f939be6c388
ManavJoshi <manavjoshi154@gmail.com>
2024-05-03 16:16:25 +0530

    Updated color scheme, resume, experience and projects

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -1,31 +1,40 @@
-import React from 'react'
+import React from "react";
 import Card from "./Card";
 const Experience = () => {
-    return (
-        <>
-            <center><h2 className='titleproj text-light'><u>Experience</u></h2></center>
-            <br />
-            <Card
-                title="Web Development Intern @Internauts Infotech"
-                description="Led the team in developing the project “digiScholar”, a scholarship portal using MERN Stack. Designed schema for
+  return (
+    <>
+      <center>
+        <h2 className="text-light">
+          <u className="titleproj">Experience</u>
+        </h2>
+      </center>
+      <br />
+      <Card
+        title="Full Stack Intern @SocialPilot Solutions LLP"
+        description="Worked on frontend & backend both, improved UI for auth module pages as per the requirements, migrated queries to new database which resulted into ~60% of improvement"
+        technologies="KoaJS, ReactJS, Redux, MongoDB"
+        className="card"
+      />
+      <Card
+        title="Web Development Intern @Internauts Infotech"
+        description="Led the team in developing the project “digiScholar”, a scholarship portal using MERN Stack. Designed schema for
                 the project and developed APIs for seamless CRUD operations. Successfully Integrated frontend and backend components of the project"
-                technologies="MERN Stack, Git, GitHub, Bootstrap"
-                className="card"
-            />
-            <Card
-                title="Web Development Intern @Thinkbeat Solutions"
-                description="Worked As Web Development Intern in a startup named Thinkbeat Solutions. Worked on technologies like React and Magento2"
-                technologies="HTML CSS JS React Magento2 Servers"
-                className="card"
-            />
-            <Card
-                title="Lead - Google Developer Student Clubs BVM"
-                description="I got selected as Google Developer Student Clubs Lead for our college for tenure 2022-23. I am leading the team of 11 members. We organise workshops, seminars and events on various technologies"
-                className="card"
-            />
-
-        </>
-    )
-}
+        technologies="MERN Stack, Git, GitHub, Bootstrap"
+        className="card"
+      />
+      <Card
+        title="Web Development Intern @Thinkbeat Solutions"
+        description="Worked As Web Development Intern in a startup named Thinkbeat Solutions. Worked on technologies like React and Magento2"
+        technologies="HTML CSS JS React Magento2 Servers"
+        className="card"
+      />
+      <Card
+        title="Lead - Google Developer Student Clubs BVM"
+        description="I got selected as Google Developer Student Clubs Lead for our college for tenure 2022-23. I am leading the team of 11 members. We organise workshops, seminars and events on various technologies"
+        className="card"
+      />
+    </>
+  );
+};
 
 export default Experience;
--- src/Data/projects.js
+++ src/Data/projects.js
@@ -1,76 +1,67 @@
 const projects = [
   {
+    title: "Curio - Spark Your Curiosity!",
+    description:
+      "Curio is a community driven QnA platform where users can ask questions, answer questions, upvote and downvote questions and answers.It has feature of content moderation as well. It is a full-stack web application built using MERN stack.",
+    technologies: ["MERN Stack", "Redux", "React-BootStrap"],
+    code_url: "https://github.com/manavjoshi111/curio",
+    live_url: "https://app-curio.vercel.app/",
+  },
+  {
     title: "myCV - Professional Resume Builder",
-    description: "myCV is a professional resume builder web app which helps the user making their resume. User need to create an account and fill the form to make their resume. The resume can be downloaded as a pdf file.",
-    technologies: [
-      "MERN Stack "
-    ],
+    description:
+      "myCV is a professional resume builder web app which helps the user making their resume. User need to create an account and fill the form to make their resume. The resume can be downloaded as a pdf file.",
+    technologies: ["MERN Stack "],
     code_url: "https://github.com/manavjoshi111/resume_builder_frontend",
-    live_url: "https://resume-builder-frontend.onrender.com/"
+    live_url: "https://resume-builder-frontend.onrender.com/",
   },
   {
     title: "CryptoX - A Cryptocurrency Tracker",
-    description: "CryptoX is a real-time tracker for multiple cryptocurrencies. It enables users to monitor prices, view ticker charts, simulate transactions with dummy funds, and review transaction history for evaluating profit/loss.",
-    technologies: [
-      "MERN Stack ",
-      "Bootstrap ",
-      "ChartJS ",
-    ],
+    description:
+      "CryptoX is a real-time tracker for multiple cryptocurrencies. It enables users to monitor prices, view ticker charts, simulate transactions with dummy funds, and review transaction history for evaluating profit/loss.",
+    technologies: ["MERN Stack ", "Bootstrap ", "ChartJS "],
     code_url: "https://github.com/ManavJoshi111/stock_project_frontend",
-    live_url: "https://tinyurl.com/cryptoxTrading"
+    live_url: "https://tinyurl.com/cryptoxTrading",
   },
   {
     title: "VendorVortex",
-    description: "The project is designed to help local vendors grow their small businesses by providing them a platform to showcase their shops and communities and plans. Customers can easily discover these shops, connect with their local community, explore current discount plans, and even share their experiences on that platform with rating of service of that shop.",
-    technologies: [
-      "MERN Stack ",
-      "Bootstrap"
-    ],
+    description:
+      "The project is designed to help local vendors grow their small businesses by providing them a platform to showcase their shops and communities and plans. Customers can easily discover these shops, connect with their local community, explore current discount plans, and even share their experiences on that platform with rating of service of that shop.",
+    technologies: ["MERN Stack ", "Bootstrap"],
     code_url: "https://github.com/ManavJoshi111/VendorVortex",
-    live_url: "https://vendorvortex.us/"
+    live_url: "https://vendorvortex.us/",
   },
   {
     title: "Cronux - A Streaming Platform",
-    description: "Cronux is a full- stack OTT(Over - the - Top) platform that offers a wide range of movies and TV shows for users to enjoy.This project was collaboratively developed by a team of two talented individuals.With Cronux, users can create their personal accounts and indulge in their preferred shows, ensuring a tailored and immersive entertainment experience.",
-    technologies: [
-      "HTML ",
-      "CSS ",
-      "JavaScript ",
-      "PHP ",
-      "MySQL "
-    ],
+    description:
+      "Cronux is a full- stack OTT(Over - the - Top) platform that offers a wide range of movies and TV shows for users to enjoy.This project was collaboratively developed by a team of two talented individuals.With Cronux, users can create their personal accounts and indulge in their preferred shows, ensuring a tailored and immersive entertainment experience.",
+    technologies: ["HTML ", "CSS ", "JavaScript ", "PHP ", "MySQL "],
     code_url: "https://github.com/ManavJoshi111/Cronux",
-    live_url: ""
+    live_url: "",
   },
   {
     title: "WeCheck - A Weather App",
-    description: "WeCheck is a User friendly weather app which shows the weather of the specified location. It shows the min/max temperature observed at the location specifed by the user.",
-    technologies: [
-      "ReactJS"
-    ],
+    description:
+      "WeCheck is a User friendly weather app which shows the weather of the specified location. It shows the min/max temperature observed at the location specifed by the user.",
+    technologies: ["ReactJS"],
     code_url: "https://github.com/ManavJoshi111/WeatherAppWithReact",
-    live_url: ""
+    live_url: "",
   },
   {
     title: "Spotified - A Music Player Web App",
-    description: "I made a basic web app that gives the same experience as the music app giants like Spotify, Ganna, etc. Users can navigate through the UI easily and enjoy their preferred music.",
-    technologies: [
-      "HTML ",
-      "CSS ",
-      "JavaScript"
-    ],
+    description:
+      "I made a basic web app that gives the same experience as the music app giants like Spotify, Ganna, etc. Users can navigate through the UI easily and enjoy their preferred music.",
+    technologies: ["HTML ", "CSS ", "JavaScript"],
     code_url: "https://github.com/ManavJoshi111/Spotified",
-    live_url: "https://manavjoshi111.github.io/Spotified/"
+    live_url: "https://manavjoshi111.github.io/Spotified/",
   },
   {
     title: "Contest Notifier - A Discord Bot",
-    description: "It is a discord bot I made for GDSC BVM discord server, the bot notifies users about the upcoming contests in next 24 hours",
-    technologies: [
-      "JavaScript ",
-      "Discord JS"
-    ],
+    description:
+      "It is a discord bot I made for GDSC BVM discord server, the bot notifies users about the upcoming contests in next 24 hours",
+    technologies: ["JavaScript ", "Discord JS"],
     code_url: "https://github.com/ManavJoshi111/Contest_Notifier_Bot",
   },
-]
+];
 
 export default projects;
