04548cd0104ae08c0971c35dc9696f3ef9c3b2a1
Manav H Joshi <73978411+ManavJoshi111@users.noreply.github.com>
2026-08-29 14:34:05 +0530

    CHORE: stop tracking visitors

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import App from "./components/App";
-import { initTracking } from "./lib/tracker";
+// import { initTracking } from "./lib/tracker";
 
 createRoot(document.getElementById("root")!).render(<App />);
 
-initTracking();
+// initTracking();
